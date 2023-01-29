af366922ad0d6957605f13d024e774d052b72367
Dan P <me@dipanp.com>
2023-01-29 02:32:13 -0600

    Initialized Genre endpoint

---
+++ src/main/java/com/gcp/springboot/jamsession/api/genre/Genre.java
@@ -0,0 +1,36 @@
+package com.gcp.springboot.jamsession.api.genre;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity // keyword that tells the Spring Boot that the following
+        // class should be considered as a table class
+public class Genre {
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long genre_id;
+    private String name;
+    public Genre() {
+    }
+
+    public long getId() {
+        return genre_id;
+    }
+
+    public void setId(long genre_id) {
+        this.genre_id = genre_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Genre(long genre_id, String name) {
+        super();
+        this.genre_id = genre_id;
+        this.name = name;
+    }
+}
--- src/main/java/com/gcp/springboot/jamsession/api/genre/GenreRepository.java
+++ src/main/java/com/gcp/springboot/jamsession/api/genre/GenreRepository.java
@@ -2,11 +2,11 @@
 // A simple repository provides all the basic methods required to
 // find, create and delete entries in the database.
 
-package com.gcp.springboot.jamsession.api.user;
+package com.gcp.springboot.jamsession.api.genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // Extend CrudRepository and create an interface of it.
 // In the params, you have to pass the name of the Java class that you want
 // to extend and type of the primary key being passed (<School, Long>).
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
