6328b29cde1e85ddcd62beb3d39b1959dd2590be
Dan P <me@dipanp.com>
2023-02-08 11:18:30 -0600

    Added @CrossOrigin property to each repository individually
    A more global method exists but didn't have immediate luck - opportunity to improve in the future if this works.

--- src/main/java/com/gcp/springboot/jamsession/api/genre/GenreRepository.java
+++ src/main/java/com/gcp/springboot/jamsession/api/genre/GenreRepository.java
@@ -1,5 +1,7 @@
 package com.gcp.springboot.jamsession.api.genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
