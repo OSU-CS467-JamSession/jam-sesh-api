1ccc575257be1ab7c09e65ece51cd51413b6ba7b
Dan P <me@dipanp.com>
2023-02-07 18:53:19 -0600

    Cleanup unused controllers

--- src/main/java/com/gcp/springboot/jamsession/api/login/LoginController.java
+++ src/main/java/com/gcp/springboot/jamsession/api/login/LoginController.java
@@ -1,11 +1,6 @@
 package com.gcp.springboot.jamsession.api.login;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LoginController {
-
-    @Autowired
-    private LoginService loginService;
 }
