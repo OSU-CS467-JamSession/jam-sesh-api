223617ebd8ad70f67461e685c9245298696ff923
Dan P <me@dipanp.com>
2023-02-13 16:46:40 -0600

    Updated Sessions to support CrossOrigin

--- src/main/java/com/gcp/springboot/jamsession/api/session/SessionRepository.java
+++ src/main/java/com/gcp/springboot/jamsession/api/session/SessionRepository.java
@@ -1,6 +1,8 @@
 package com.gcp.springboot.jamsession.api.session;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface SessionRepository extends JpaRepository<Session, Long> {
 }
