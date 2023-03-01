e18d51cad744639eb88055716a3ac453147b8a77
fitzpaca <fitzpaca@oregonstate.edu>
2023-02-28 20:25:11 -0700

    Comments endpoint created. Delete comments not working properly (omitting from postman docs for now)

---
+++ src/main/java/com/gcp/springboot/jamsession/api/comment/CommentController.java
@@ -0,0 +1,7 @@
+package com.gcp.springboot.jamsession.api.comment;
+
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+@RepositoryRestController
+public class CommentController {
+}
